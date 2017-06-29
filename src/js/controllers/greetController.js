c19031178de31a209696c914c76531289ac32dda
Joshua Bradford <jjyet@yahoo.com>
2017-06-29 09:01:43 -0400

    created a click through image gallery for client as detailed in story:https://www.pivotaltracker.com/story/show/148009927

--- src/js/controllers/greetController.js
+++ src/js/controllers/greetController.js
@@ -1,6 +1,20 @@
 function GreetController ($scope) {
-   console.log("Welcome tgreatsauseController");
+   var container = document.getElementById('splash_container');
+   var switch_background = document.getElementById('switch_background')
+   console.log(switch_background)
+   container.innerHTML = `<img src="../images/background.jpg">`
+   switch_background.addEventListener('click', flip);
+   var counter = 0;
+   function flip(event){
+     counter++;
+     if(counter > 11){
+       counter = 1
+     }
+     container.innerHTML = `<img src="../images/background${counter}.jpg">`
+     console.log("activate button")
+   }
  }
 
+GreetController.$inject = ['$scope'];
 
 export default GreetController;
