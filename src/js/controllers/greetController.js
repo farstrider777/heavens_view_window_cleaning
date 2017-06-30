3bfa02af899c861cc0925ff34969b98f34535acf
Joshua Bradford <jjyet@yahoo.com>
2017-06-30 00:08:18 -0400

    5 background pictures set to load randomly to satisfy: https://www.pivotaltracker.com/story/show/148122083

--- src/js/controllers/greetController.js
+++ src/js/controllers/greetController.js
@@ -2,12 +2,14 @@ function GreetController ($scope) {
    var container = document.getElementById('splash_container');
    var switch_background = document.getElementById('switch_background')
    console.log(switch_background)
-   container.innerHTML = `<img src="../images/background.jpg">`
+
    switch_background.addEventListener('click', flip);
-   var counter = 0;
+   var counter = Math.floor(Math.random() * 5) + 1 ;
+   console.log(counter)
+   container.innerHTML = `<img src="../images/background${counter}.jpg">`
    function flip(event){
      counter++;
-     if(counter > 11){
+     if(counter > 4){
        counter = 1
      }
      container.innerHTML = `<img src="../images/background${counter}.jpg">`
